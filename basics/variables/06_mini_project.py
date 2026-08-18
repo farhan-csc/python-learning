3a567becd33a2c7c069547df96e001913f7f452b
Muhammad Farhan <307641494+farhan-csc@users.noreply.github.com>
2026-08-18 21:02:18 +0700

    Add variables mini project

---
+++ basics/variables/06_mini_project.py
@@ -0,0 +1,11 @@
+product_name = "Glasses"
+price = 300000
+quantity = 2
+total = price * quantity
+
+print("==== RECEIPT ====")
+print(f"Product: {product_name}")
+print(f"Price: {price}")
+print(f"Quantity: {quantity}")
+print(f"Total: {total}")
+print("=================")
