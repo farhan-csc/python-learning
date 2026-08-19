997c528e388714f2eb01241246810729bc6abd6a
Muhammad Farhan <307641494+farhan-csc@users.noreply.github.com>
2026-08-19 21:21:31 +0700

    Add numbers data type practice

---
+++ basics/data_types/01_numbers.py
@@ -0,0 +1,11 @@
+age = 19
+year = 2026
+height = 173.5
+weight = 55
+temperature = 30
+
+print(f"Age: {age} {type(age)}")
+print(f"Year: {year} {type(year)}")
+print(f"Height: {height} {type(height)}")
+print(f"Weight: {weight} {type(weight)}")
+print(f"Temperature: {temperature} {type(temperature)}")
